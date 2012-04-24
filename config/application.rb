8492821f1fa14e93ab96481e98e2f9d214a7f5ae
Larry Finn <larry@cube2media.com>
2012-04-23 20:53:05 -0700

    eager load goodies

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,10 @@ module Bsp
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
+    
+    config.paths['app/manifests'] = 'app/manifests'
+    config.paths['app/manifests'].skip_eager_load!
+ 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
