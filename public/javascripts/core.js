9b840a36d0a7cfcd710c0dbb1a84f7dbfb9a7776
Bob Hanson <bob@cube2media.com>
2012-07-08 15:52:24 -0600

    IE9 Video player fix

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -142,12 +142,18 @@ var Video = {
   },
 
   playModalVideo: function() {
-    var player = _V_("modal_video");
+    var htmlTag = $("html"),
+        player = _V_("modal_video");
         playerHtml = $("#modal_video_player");
 
       playerHtml.removeClass("hidden");
       player.width(580);
       player.height(326);
+      if (htmlTag.hasClass("ie9")) {
+        console.log("ie9");
+        _V_.options.techOrder = ["flash"];
+      }
+
       player.play();
   },
 
