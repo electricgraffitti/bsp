533219e5d75fee1f82de0567a11e73581c961d98
Bob Hanson <bob@cube2media.com>
2012-08-20 07:57:23 -0600

    fixed sponsored schools to new algorithm and setup countdown days to be dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,15 @@ module ApplicationHelper
 	end
 
 	def pledge_count
-		Pledge.count ? Pledge.count : 0
+		Pledge.sum(:amount) ? (Pledge.sum(:amount)/20).to_i : 0
 	end
 
 	def pledge_amount
 		number_with_precision Pledge.sum(:amount), :precision => 2
+	end
+
+	def countdown
+		(Date.new(2012,10,1) - Date.today).to_i 
 	end
 
 end
